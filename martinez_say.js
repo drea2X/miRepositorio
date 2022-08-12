a69c1fc1a30d043d4c8d3cf5f50500870a65bee5
say2005 <iccpmartinezb27@gmail.com>
2022-08-12 12:58:59 -0400

    predecirfuncione

---
+++ martinez_say.js
@@ -0,0 +1,27 @@
+function myBirthYearFunc(){
+        console.log("Nací en " + 1980);
+    }//console.log imprime naci en 1980//
+    
+    function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+            
+            
+    var año=2005
+    undefined
+     function myBirthYearFunc(año){
+            console.log("Nací en " + año);
+     }
+    undefined
+    myBirthYearFunc(año)
+    VM195:2 Nací en 2005    
+
+    function add(num1, num2){
+            console.log("¡Sumando números!");
+            console.log("num1 is: 10" + num1);
+            console.log("num2 is:20 " + num2);
+            var sum = num1 + num2;
+            console.log(sum);
+        }
+        
+        //console.log imprime 30//
---
+++ script.js
@@ -0,0 +1,12 @@
+<ul>
+    <li>
+        <a href="home.html">Inicio</a>
+    </li>
+    <li>
+        <a href="about.html">Sobre Nosotros</a>
+    </li>
+    <li>
+        <a href="contact_us.html">Contáctanos</a>
+    </li>
+</ul>
+
